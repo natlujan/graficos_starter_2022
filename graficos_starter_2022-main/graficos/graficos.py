318244b4ffab24962ecc0f0a44b917f8a2ddb952
wera <69810414+natlujan@users.noreply.github.com>
2022-02-06 22:19:06 -0700

    Update graficos.py

--- graficos_starter_2022-main/graficos/graficos.py
+++ graficos_starter_2022-main/graficos/graficos.py
@@ -9,62 +9,90 @@ from glew_wish import *
 import glfw
 import math
 
-color = [0.6, 0.1, 0.7]
-posicion = [-0.4,0.2]
-velocidad = 0.01
+color_triangulo = [0.6, 0.9, 0.6]
+posicion_triangulo = [-0.4,0.2]
+velocidad_triangulo = 0.01
 
+color_cuadrado = [0.6, 0.4, 0.2]
+posicion_cuadrado = [0.5,0.2]
+velocidad_cuadrado = 0.01
 
 def key_callback(window, key, scancode, action, mods):
-    global color
-    global posicion
-    global velocidad
+    global color_triangulo
+    global posicion_triangulo
+    global velocidad_triangulo
 
     #Que la tecla escape cierre ventana al ser presionado
     if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
         glfw.set_window_should_close(window, 1)
     if key == glfw.KEY_R and action == glfw.PRESS:
-        color = [1.0,0.0,0.0]
+        color_triangulo = [1.0,0.0,0.0]
     if key == glfw.KEY_G and action == glfw.PRESS:
-        color = [0.0,1.0,0.0]
+        color_triangulo = [0.0,1.0,0.0]
     if key == glfw.KEY_B and action == glfw.PRESS:
-        color = [0.0,0.0,1.0]
+        color_triangulo = [0.0,0.0,1.0]
 
-    #Al presionar arriba se mueve hacia arriba
+    #Controles del triangulo
     if key == glfw.KEY_UP and (action == glfw.PRESS or glfw.REPEAT):
-        posicion[1] = posicion [1] + velocidad
-        if posicion[1] >= 1:
-            posicion[1] = -0.9999
-    #Al presionar abajo se mueve hacia abajo
+        posicion_triangulo[1] = posicion_triangulo [1] + velocidad_triangulo
+        if posicion_triangulo[1] >= 1:
+            posicion_triangulo[1] = -0.9999
     if key == glfw.KEY_DOWN and (action == glfw.PRESS or glfw.REPEAT):
-        posicion[1] = posicion [1] - velocidad
-        if posicion[1] <= -1:
-            posicion[1] = 0.9999
-    #Al presionar derecha se mueve hacia derecha
+        posicion_triangulo[1] = posicion_triangulo [1] - velocidad_triangulo
+        if posicion_triangulo[1] <= -1:
+            posicion_triangulo[1] = 0.9999
     if key == glfw.KEY_RIGHT and (action == glfw.PRESS or glfw.REPEAT):
-        posicion[0] = posicion [0] + velocidad
-        if posicion[0] >= 1:
-            posicion[0] = -0.9999
-    #Al presionar izquierda se mueve hacia izquierda
+        posicion_triangulo[0] = posicion_triangulo [0] + velocidad_triangulo
+        if posicion_triangulo[0] >= 1:
+            posicion_triangulo[0] = -0.9999
     if key == glfw.KEY_LEFT and (action == glfw.PRESS or glfw.REPEAT):
-        posicion[0] = posicion [0] - velocidad
-        if posicion[0] <= -1:
-            posicion[0] = 0.9999
+        posicion_triangulo[0] = posicion_triangulo [0] - velocidad_triangulo
+        if posicion_triangulo[0] <= -1:
+            posicion_triangulo[0] = 0.9999
+
+    global color_cuadrado
+    global posicion_cuadrado
+    global velocidad_cuadrado
+
+    #Controles del cuadrado
+    if key == glfw.KEY_W and (action == glfw.PRESS or glfw.REPEAT):
+        posicion_cuadrado[1] = posicion_cuadrado [1] + velocidad_cuadrado
+        if posicion_cuadrado[1] >= 1:
+            posicion_cuadrado[1] = -0.9999
+    if key == glfw.KEY_S and (action == glfw.PRESS or glfw.REPEAT):
+        posicion_cuadrado[1] = posicion_cuadrado [1] - velocidad_cuadrado
+        if posicion_cuadrado[1] <= -1:
+            posicion_cuadrado[1] = 0.9999
+    if key == glfw.KEY_D and (action == glfw.PRESS or glfw.REPEAT):
+        posicion_cuadrado[0] = posicion_cuadrado [0] + velocidad_cuadrado
+        if posicion_cuadrado[0] >= 1:
+            posicion_cuadrado[0] = -0.9999
+    if key == glfw.KEY_A and (action == glfw.PRESS or glfw.REPEAT):
+        posicion_cuadrado[0] = posicion_cuadrado [0] - velocidad_cuadrado
+        if posicion_cuadrado[0] <= -1:
+            posicion_cuadrado[0] = 0.9999
 
 def draw():
-    global color
-
+    global color_triangulo
+    #TRIANGULO
     glPushMatrix()
-    glTranslatef(posicion[0], posicion[1], 0.0)
+    glTranslatef(posicion_triangulo[0], posicion_triangulo[1], 0.0)
     glBegin(GL_TRIANGLES)
-
-    #Establecer color
-    glColor3f(color[0],color[1],color[2])
-
-    #Manda vertices a dibujar
-    glVertex3f(-0.08,-0.08,0)
+    glColor3f(color_triangulo[0],color_triangulo[1],color_triangulo[2])
+    glVertex3f(-0.2,-0.1,0)
     glVertex3f(0,0.08,0)
     glVertex3f(0.08,-0.08,0)
-
+    glEnd()
+    glPopMatrix()
+    #CUADRADO
+    glPushMatrix()
+    glTranslatef(posicion_cuadrado[0], posicion_cuadrado[1], 0.0)
+    glBegin(GL_QUADS)
+    glColor3f(color_cuadrado[0],color_cuadrado[1],color_cuadrado[2])
+    glVertex3f(0.1,0.1,0)
+    glVertex3f(0,0.1,0)
+    glVertex3f(0,0,0)
+    glVertex3f(0.1,0,0)
     glEnd()
     glPopMatrix()
 
